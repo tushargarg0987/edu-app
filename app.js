e903898d714a637c3dc6f076f6749f697b3bc410
Tushar <tushargarg0987@gmail.com>
2023-05-11 12:45:08 +0530

    Enable for json format

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(
     session({
         secret: "livestreamApp",
